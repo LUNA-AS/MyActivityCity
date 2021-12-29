7f0aececb5f43d3b7b07c5b25e4451413eed6b61
LUNA-AS <luabousaad@gmail.com>
2021-12-29 15:05:33 +0000

    Created new task activity

---
+++ app/src/main/java/com/example/myactivitycity/MyAdapter.java
@@ -0,0 +1,42 @@
+package com.example.myactivitycity;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView titleOutput;
+        TextView descriptionOutput;
+        TextView timeOutput;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            titleOutput = itemView.findViewById(R.id.titleOutput);
+            descriptionOutput = itemView.findViewById(R.id.descriptionOutput);
+            timeOutput = itemView.findViewById(R.id.timeOutput);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myactivitycity/Task.java
@@ -0,0 +1,33 @@
+package com.example.myactivitycity;
+
+import io.realm.RealmObject;
+
+public class Task extends RealmObject {
+    String title;
+    String description;
+    long timeCreated;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public long getTimeCreated() {
+        return timeCreated;
+    }
+
+    public void setTimeCreated(long timeCreated) {
+        this.timeCreated = timeCreated;
+    }
+}
