6016216651c59a298578a4428d7053dadbe2acab
LUNA-AS <luabousaad@gmail.com>
2021-12-29 15:51:52 +0000

    Added add task button to main activity and linked it to new task activity

--- app/src/main/java/com/example/myactivitycity/NewTask.java
+++ app/src/main/java/com/example/myactivitycity/NewTask.java
@@ -3,6 +3,10 @@ package com.example.myactivitycity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class NewTask extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class NewTask extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_task);
+
+        EditText titleInput = findViewById(R.id.titleInput);
+        EditText descriptionInput = findViewById(R.id.descriptionInput);
+        Button saveBtn = findViewById(R.id.saveTask);
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String title = titleInput.getText().toString();
+                String description = descriptionInput.getText().toString();
+
+                if(!title.equals("title")  && !title.equals("")){
+
+                }else{
+                    titleInput.requestFocus();
+                    Toast.makeText(NewTask.this,"Invalid title",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
     }
 }
