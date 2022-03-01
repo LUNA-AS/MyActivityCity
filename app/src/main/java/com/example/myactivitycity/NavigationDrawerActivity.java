56efb3f5e7abcc2cb6e73b0ead1586bd1d9ed7f3
LUNA-AS <luabousaad@gmail.com>
2022-03-01 10:43:19 +0000

    Changed navigation

--- app/src/main/java/com/example/myactivitycity/NavigationDrawerActivity.java
+++ app/src/main/java/com/example/myactivitycity/NavigationDrawerActivity.java
@@ -40,7 +40,7 @@ public class NavigationDrawerActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_message)
+                R.id.nav_home, R.id.nav_goals, R.id.nav_message, R.id.nav_schedule, R.id.nav_user_guide, R.id.nav_city)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_navigation_drawer);
---
+++ app/src/main/java/com/example/myactivitycity/ui/schedule/ScheduleFragment.java
@@ -0,0 +1,36 @@
+package com.example.myactivitycity.ui.schedule;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import com.example.myactivitycity.databinding.FragmentScheduleBinding;
+
+public class ScheduleFragment extends Fragment {
+
+    private FragmentScheduleBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        ScheduleViewModel scheduleViewModel =
+                new ViewModelProvider(this).get(ScheduleViewModel.class);
+
+        binding = FragmentScheduleBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.scheduleText;
+        scheduleViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
